c0497ec6f22a4c44aa3a1b4430a0871ff7d0964e
geethsheheran <gs.sheheran@gmail.com>
2025-02-19 13:32:39 +0530

    title-add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,11 @@ import "./globals.css";
 import dynamic from "next/dynamic";
 import Navbar from "@/components/navbar";
 import ScrollSection from "@/components/ScrollSection";
-import {BackgroundLinesDemo} from "@/components/BackgroundLinesDemo";
-import {WobbleCardDemo} from "@/components/WobbleCardDemo";
-import {TimelineDemo} from "@/components/TimelineDemo";
-import {ExpandableCardDemo} from "@/components/ExpandableCardDemo";
+import { BackgroundLinesDemo } from "@/components/BackgroundLinesDemo";
+import { WobbleCardDemo } from "@/components/WobbleCardDemo";
+import { TimelineDemo } from "@/components/TimelineDemo";
+import { ExpandableCardDemo } from "@/components/ExpandableCardDemo";
 import Footer from "@/components/Footer";
-
 
 const WalletProvider = dynamic(() => import("@/components/WalletProvider"), {
   ssr: false,
@@ -18,31 +17,35 @@ const WalletProvider = dynamic(() => import("@/components/WalletProvider"), {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>The Birds of Pamparigouste</title>
+        <link rel="icon" href="/phi-logo-transparent.png" />
+      </head>
       <body>
         <div>
-          <Navbar/>
+          <Navbar />
         </div>
-        <div className="h-screen ">
-        <BackgroundLinesDemo/>
-      </div>
-      <div className="w-full">
-        <ScrollSection />
-      </div>
-      <div className="bg-black">
-        <WobbleCardDemo />
-      </div>
-      <div className="bg-black">
-        <WalletProvider >{children}</WalletProvider>
+        <div className="h-screen">
+          <BackgroundLinesDemo />
+        </div>
+        <div className="w-full">
+          <ScrollSection />
+        </div>
+        <div className="bg-black">
+          <WobbleCardDemo />
+        </div>
+        <div className="bg-black">
+          <WalletProvider>{children}</WalletProvider>
         </div>
         <div>
-      <TimelineDemo />
-      </div>
-      <div  className="bg-black py-24">
-      <ExpandableCardDemo/>
-      </div>
-      <div>
-        <Footer/>
-      </div>
+          <TimelineDemo />
+        </div>
+        <div className="bg-black py-24">
+          <ExpandableCardDemo />
+        </div>
+        <div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
